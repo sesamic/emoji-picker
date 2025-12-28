4e246614d822d9bb3edd5395fd395fbc4a68e235
uukelele-scratch <robustrobot11@gmail.com>
2025-12-28 09:43:43 +0000

    scope CSS

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 import '$lib/styles.css';
-import '$lib/styles.purged.css';
+// import '$lib/styles.purged.css';
+import '$lib/styles.scoped.css';
 export { default as EmojiPicker } from './index.svelte';
